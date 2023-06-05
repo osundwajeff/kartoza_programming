bd0cc91d94ee9a5b559631b0358fc853ba6f0944
Jeff Osundwa <mulamajeff@tuta.io>
2023-06-05 12:01:10 +0300

    major modifications

--- geometry_validator/geom_validator.py
+++ geometry_validator/geom_validator.py
@@ -2,32 +2,82 @@
 """Geomtetry point validator function"""
 
 
-lat = float(input("Enter Latitude: "))
-lon = float(input("Enter Longitude: "))
+#initialize empty list
+data = []
 
 def geom_validator(lat, lon):
     """validates if geometry is valid"""
 
     status = False
+    reason = """Latitude and Longitude must be within valid range, (Latitude[-90 to 90] and Longitude[-180 to 180])."""
 
-    if -90 <= lat <= 90 and -180 <= lon <=180:
+    if (-90 <= lat <= 90) and (-180 <= lon <=180):
         status = True
+        reason = "Latitude and Longitude is within valid range."
 
     if status:
         print("Point is valid")
+        print(reason)
     else:
         print("Point is invalid")
-    
+        print(reason)
 
-    print(status)
+    data.append([lat, lon, status, reason])
 
 def check_history():
     """returns history"""
-    pass
+    
+    if not data:
+        print("""
+            ---------------
+            No history yet!
+            ---------------""")
+    else:
+        print(data)
 
 def end_session():
     """to end the session"""
     pass
 
-
-geom_validator(lat, lon)
+active_session = True
+while active_session:
+    print("""
+        ------------------
+        |      Menu      |
+        ------------------
+        1. Validate Point
+        2. Check History
+        3. End
+        """)
+    choice = int(input("Enter choice:"))
+    
+    if choice == 1:
+        #input
+        print("""
+            ------------------------------------
+            Enter Latitude and Longitude values!
+            ------------------------------------""")
+        lat = float(input("Enter Latitude: "))
+        lon = float(input("Enter Longitude: "))
+        print("""
+            ---------
+            Execution!
+            ---------""")
+        geom_validator(lat, lon)
+    elif choice == 2:
+        print("""
+            ------------------
+            Retrieving history!
+            ------------------""")
+        check_history()
+    elif choice == 3:
+        print("""
+            ---------------
+            Ending session!
+            ---------------""")
+        break
+    else:
+        print("""
+            -----------------------
+            Command does not exist!
+            -----------------------""")
